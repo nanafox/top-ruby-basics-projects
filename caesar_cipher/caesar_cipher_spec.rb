a6946eb0920b60da56dd2b50cceb59290b2d7713
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-07 17:01:15 +0000

    Add tests for Caesar Cipher implementation

---
+++ caesar_cipher/caesar_cipher_spec.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative 'caesar_cipher'
+
+RSpec.describe 'Caesar Cipher' do
+  describe 'Cipher with shift factor 3' do
+    it 'correctly ciphers "hello world"' do
+      expect(caesar_cipher('hello world', 3)).to eq('khoor zruog')
+    end
+
+    it 'correctly ciphers a fairly lengthy string' do
+      string = 'There are a lot things I would really want to do, but things take time.'
+      expected_output = 'Wkhuh duh d orw wklqjv L zrxog uhdoob zdqw wr gr, exw wklqjv wdnh wlph.'
+      expect(caesar_cipher(string, 3)).to eq(expected_output)
+    end
+  end
+
+  describe 'Cipher with shift factor 5' do
+    it 'returns an empty string when no string is provided' do
+      expect(caesar_cipher('', 5)).to eq('')
+    end
+
+    it 'correctly ciphers "Odin"' do
+      expect(caesar_cipher('Odin', 5)).to eq('Tins')
+    end
+
+    it 'correctly ciphers text with symbols' do
+      expect(caesar_cipher('Hello, world!', 5)).to eq('Mjqqt, btwqi!')
+    end
+
+    it 'can handle a fancy text with my code name' do
+      expect(caesar_cipher('I am theLazyProgrammer^_^')).to eq('N fr ymjQfedUwtlwfrrjw^_^')
+    end
+  end
+end
