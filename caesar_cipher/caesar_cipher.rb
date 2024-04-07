e813f7b8fd566e33151c8520919f6ec4af6ef2b7
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-07 16:28:36 +0000

    Add Caesar Cipher implementation

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def letter?(char)
+  char.match?(/[a-zA-Z]/)
+end
+
+def cipher(char, shift_factor)
+  char.ord + shift_factor
+end
+
+# Shifts a character by a given factor according to the Caesar Cipher.
+# The shift wraps around the alphabet, so 'z' shifted by 1 becomes 'a', and 'Z' shifted by 1 becomes 'A'.
+#
+# @param char [String] the character to be shifted. Must be a single character string.
+# @param shift_factor [Integer] the number of positions to shift the character in the alphabet.
+# @return [String] the shifted character.
+def shift_char(char, shift_factor)
+  start_char = char == char.upcase ? 'A' : 'a'
+  last_char = char == char.upcase ? 'Z' : 'z'
+
+  shifted_char = cipher(char, shift_factor)
+  shifted_char = ((shifted_char - start_char.ord) % 26) + start_char.ord if shifted_char > last_char.ord
+
+  shifted_char.chr
+end
+
+# Returns the Caesar Cipher representation of a string.
+#
+# @param string [String] The string to cipher.
+# @param shift_factor [Integer] The shift factor
+#
+# @return [String] The ciphered text.
+def caesar_cipher(string, shift_factor)
+  string.chars.map { |char| letter?(char) ? shift_char(char, shift_factor) : char }.join
+end
